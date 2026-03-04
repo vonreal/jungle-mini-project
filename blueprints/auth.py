c33e38b1d5f5584859d6777d5796f9bfeba6b2a5
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-03-04 23:05:41 +0900

    로그인 마이페이지1차
    로그인 마이페이지1차

--- blueprints/auth.py
+++ blueprints/auth.py
@@ -1,3 +1,92 @@
-from flask import Blueprint
+from flask import Flask, render_template, jsonify, request,Blueprint,make_response
+from db import db
+import hashlib
+from datetime import datetime
+import re
+import jwt
 
 bp = Blueprint('auth', __name__)
+
+@bp.route('/auth/register', methods=['post'])
+def create_id():
+    #1 아이디 비번 비번확인 닉네임 받아
+
+    new_id_receive = request.form['new_id_give']
+    new_password_receive =request.form['new_password_give']
+    new_nickname_receive =request.form['new_nickname_give']
+
+    #3 아이디,닉네임 중복과 정규형 맞췄늕 확인
+    id_dup = db.user.find_one({'username':new_id_receive})
+    nickname_dup = db.user.find_one({'nickname':new_nickname_receive})
+
+    if  id_dup is not None:
+        return jsonify({'result': 'fail', 'msg': "중복된 아이디 입니다"})
+    if nickname_dup is not None:
+        return jsonify({'result': 'fail', 'msg': "중복된 닉네임 입니다"})
+    
+    #아이디,비번,닉네임 정규식 확인
+    id_regex = r'^[a-z0-9_-]{5,20}$'
+    if not re.match(id_regex, new_id_receive):
+        return jsonify({'result': 'fail', 'msg': "아이디 형식이 올바르지 않습니다."})
+
+    password_regex = r'^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$'
+    if not re.match(password_regex, new_password_receive):
+        return jsonify({'result': 'fail', 'msg': "비밀번호 형식이 올바르지 않습니다."})
+    
+    nickname_regex = r'^[a-zA-Z가-힣]{1,16}$'
+    if not re.match(nickname_regex, new_nickname_receive):
+        return jsonify({'result': 'fail', 'msg': "닉네임 형식이 올바르지 않습니다."})
+    
+
+    #4 비밀번호를 해싱
+    new_hash_passward = hashlib.sha256(new_password_receive.encode('utf-8')).hexdigest()
+
+    #5 디비에 유저객체를 만들어 저장해
+    doc = {
+        'username': new_id_receive,
+        'password': new_hash_passward,
+        'nickname': new_nickname_receive,
+        'create_date':datetime.now().strftime('%Y%m%d%H%M%S')
+    }
+
+    db.user.insert_one(doc)
+
+    #6 완료 응답
+    return jsonify({'result': 'success', 'msg': "회원가입 완료되었습니다!"})
+
+
+
+
+@bp.route('/auth/login', methods=['post'])
+def check_login():
+
+    #1단 아이디랑 비번을 받아
+    id_receive = request.form['id_give']
+    password_receive =request.form['passward_give']
+
+    #2제 비번을 암호화해
+    hash_password = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+
+    #3. 아이디랑 비번을 디비랑 조회를 해
+    results = db.user.find_one({'username':id_receive},{'password':hash_password})
+
+    #4.확인을 하고 토큰을 주거나 알림을 줘
+
+    SECRET_KEY = "sheep"
+    if results is not None:
+
+        payload = {
+            'id': id_receive,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=60 * 60)  # 1시간 유효
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        res = make_response(jsonify({'result': 'success', 'msg': '로그인 성공!'}))
+
+        # 클라이언트 컴퓨터 쿠기에 토큰을 저장해
+        res.set_cookie('token', token)
+
+        return res
+    
+    else:
+        return jsonify({'result': 'fail','msg': '로그인 실패!'})
--- blueprints/mypage.py
+++ blueprints/mypage.py
@@ -1,3 +1,96 @@
-from flask import Blueprint
+from flask import Flask, render_template, jsonify, request,Blueprint
+from db import db
+from datetime import datetime
+import re
+import jwt
+import os
+from utils import handle_image
 
 bp = Blueprint('mypage', __name__)
+SECRET_KEY = "sheep"
+
+@bp.route('/mypage/update_mypage', methods=['post'])
+def change_pofile_img_path():
+
+    # 1. 클라이언트 쿠키를 꺼내
+    token_receive = request.cookies.get('mytoken')
+
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        # 토큰에 적힌 id 가져오기
+        user_id = payload['id']
+    except jwt.ExpiredSignatureError:
+        # 토큰의 유효기간이 끝난 경우
+        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
+    except jwt.exceptions.DecodeError:
+        # 토큰이 뭔가 잘못됨
+        return jsonify({'result': 'fail', 'msg': '로그인이 필요합니다.'})
+
+
+    new_nickname = request.form.get('nickname_give')
+    saved_file_path = handle_image.save_image(request.files, type="profile")
+    # file = request.files.get('file_give')
+    # if not file:
+    #     return jsonify({'result': 'fail', 'msg': '사진 파일이 없습니다.'})
+    
+    # extension = file.filename.split('.')[-1]
+    # now_time = datetime.now().strftime('%Y%m%d%H%M%S')
+    # file_name = f"{user_id}_{now_time}.{extension}"
+
+    # save_path = os.path.join('static/image', file_name)
+    # file.save(save_path)
+
+    
+
+    db.user.update_one(
+        {'username': user_id},
+        {'$set': {
+            'nickname': new_nickname,
+            'profile_img': saved_file_path
+        }}
+    )
+
+    return jsonify({'result': 'success', 'msg': '프로필 수정 완료!'})
+
+@bp.route('/mypage', methods=['GET'])
+def get_mypage():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_id = payload['id']
+    except jwt.ExpiredSignatureError:
+        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
+    except jwt.exceptions.DecodeError:
+        return jsonify({'result': 'fail', 'msg': '로그인이 필요합니다.'})
+
+
+    user = db.user.find_one({'username': user_id}, {'_id': False, 'password': False})
+    if not user:
+        return jsonify({'result': 'fail', 'msg': '사용자를 찾을 수 없습니다.'})
+    
+    user_feeds = list(db.feeds.find({'username': user_id}).sort('create_date', -1))
+
+    feeds_list = []
+    total_likes = 0
+
+
+    for f in user_feeds:
+
+        #여기에 좋아요를 가져 올 수 있을꺼 같은데
+        
+
+        feeds_list.append({
+            'feed_id': str(f['_id']),
+            'feed_img': f.get('image_path', '')
+        })
+
+
+    
+    # 리스폰스 조
+    return jsonify({
+        'profile_img': user.get('profile_img_path', 'default_profile.png'),
+        'nickname': user.get('nickname'),
+        'likes': total_likes,
+        'feed_total': len(feeds_list), 
+        'feeds': feeds_list
+    })
